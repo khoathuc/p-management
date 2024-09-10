c2a520eed591074e5e676a78935216f2be4e86cf
NicolasWillyam <phambahuyhoang0@gmail.com>
2024-09-10 21:05:01 +0700

    Fix conflicts

--- client/auth-utils.ts
+++ client/auth-utils.ts
@@ -1,15 +1,10 @@
 import axios from "axios";
 
-<<<<<<< HEAD
 // const API_BASE_URL = "http://localhost:3001";
-=======
-const API_BASE_URL = "http://localhost:3001"; // Adjust based on your API URL
->>>>>>> 9ac3383ab1c8fc49fb6a99b949833bf8c88a22fb
 
 export const isAuthenticated = async (): Promise<boolean> => {
   try {
     // Example: Check if the user is authenticated by making a request to an endpoint
-<<<<<<< HEAD
     const response = await axios.get(
       `${process.env.API_BASE_URL}/auth/status`,
       {
@@ -17,12 +12,6 @@ export const isAuthenticated = async (): Promise<boolean> => {
         // headers: { Authorization: `Bearer ${token}` },
       }
     );
-=======
-    const response = await axios.get(`${API_BASE_URL}/auth/status`, {
-      // Include authentication token if needed
-      // headers: { Authorization: `Bearer ${token}` },
-    });
->>>>>>> 9ac3383ab1c8fc49fb6a99b949833bf8c88a22fb
 
     return response.status === 200; // Assuming 200 means authenticated
   } catch (error) {
--- client/server/login.ts
+++ client/server/login.ts
@@ -5,10 +5,6 @@ import * as z from "zod";
 import { LoginSchema } from "../schemas";
 import axios from "axios";
 import { ENDPOINTS } from "../constants/api";
-<<<<<<< HEAD
-=======
-import { signIn } from "../auth";
->>>>>>> 9ac3383ab1c8fc49fb6a99b949833bf8c88a22fb
 import { redirect } from "next/navigation";
 import { DEFAULT_LOGIN_REDIRECT } from "../routes";
 import { AuthError } from "next-auth";
